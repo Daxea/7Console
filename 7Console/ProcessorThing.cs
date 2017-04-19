4b59228e247811114c5c34c0756b06c065a3fb71
Eddy Pinkston <eddy.ews@gmail.com>
2017-04-19 13:43:07 -0500

    7C - Add code to play with C# 7 features

---
+++ 7Console/ProcessorThing.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7Console
+{
+    public class ProcessorThing
+    {
+        public (int result, string message, string error) Process(object data)
+        {
+            if (data is string s && s.Any(c => c == '!'))
+                return (0, $"{s}? Wow!", string.Empty);
+            if (data is Task t)
+            {
+                t.Wait();
+                return (1, "Task Completed", $"{(t.IsFaulted ? "with" : "without")} fault");
+            }
+            return (-1, "Error", "Unknown Object Data Cannot Be Processed.");
+        }
+    }
+}
---
+++ 7Console/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace _7Console
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            void WriteLine(string text) => Console.WriteLine(text);
+
+            WriteLine("C# 7 Feature Exploration");
+
+            (int id, string name) GetIdentity() => (id: 10_24_42_80, name: "Eddy");
+            var (id, name) = GetIdentity();
+            WriteLine($"\tId: {id}\n\tName: {name}");
+            Console.ReadKey();
+
+            var processor = new ProcessorThing();
+            Task task = Task.Run(() =>
+            {
+                var array = new[] { "char", "int", "hex", "bin" };
+                for (var i = 0; i < 10_000; i++)
+                {
+                    if (i % 5 == 0)
+                        Console.Clear();
+                    Console.Write($"{array.AtRandom()}* rando -set:'{i}'\n");
+                }
+                Console.Clear();
+            });
+            PrintResults(processor.Process(task));
+            PrintResults(processor.Process("Whoa!"));
+            PrintResults(processor.Process(42));
+            Console.ReadKey();
+
+            Console.Clear();
+            CalculateUnits(2f, out var inRadius, out var circumRadius);
+            PrintResults((result: 69, $"{inRadius}, {circumRadius}!", string.Empty));
+            Console.ReadKey();
+
+            void PrintResults((int result, string message, string error) results) =>
+                WriteLine($"Results: {results.result}, {results.message} {results.error}");
+        }
+
+        private static Random s_rand = new Random();
+
+        public static string AtRandom(this string[] array)
+        {
+            return array[s_rand.Next(0, array.Length)];
+        }
+
+        public static void CalculateUnits(float length, out float inRadius, out float circumRadius)
+        {
+            inRadius = length / (float)Math.Sqrt(3);
+            circumRadius = inRadius * 1.5f;
+        }
+    }
+}
